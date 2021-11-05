66baf974dd2e17bfc4ea9c31e60a83633be1b208
Wladyk <Wladyk@users.noreply.github.com>
2021-11-05 06:15:12 -0300

    Update settings.py

--- proyectouno/settings.py
+++ proyectouno/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'softproductos',
         'USER' : 'postgres',
-        'PASSWORD' : 'vordank',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '5432'
     }
