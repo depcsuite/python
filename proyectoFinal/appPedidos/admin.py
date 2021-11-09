bd3484256aa7e31c22c152026e24e30a7e3168a4
Wladyk <Wladyk@users.noreply.github.com>
2021-11-09 19:03:04 -0300

    Delete proyectoFinal directory

--- proyectoFinal/appPedidos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from appPedidos.models import Marca, ProductoPedido, Producto, Pedido
-
-# Register your models here.
-admin.site.register(Producto)
-admin.site.register(Pedido)
-admin.site.register(ProductoPedido)
-admin.site.register(Marca)
--- proyectoFinal/appPedidos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-]
