f2a6392dfd621b0d324c7423540f5d3416ecedd6
wladyk <cristianmitas@gmail.com>
2021-12-03 03:28:39 -0300

    Se eliminaron carpetas innecesarias

--- finalcurso/sistemapedidos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from sistemapedidos.models import Producto, Marca, Pedido, ProductoPedido
-
-# Register your models here.
-admin.site.register(Producto)
-admin.site.register(Marca)
-admin.site.register(Pedido)
-admin.site.register(ProductoPedido)
